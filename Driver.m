532d1cba9a4d3b4571cb83ecc4bad5d4042ce4f2
Ludovica Scaffidi <ludovicascaffidi@MacBook-Air-di-Ludovica.local>
2025-05-31 19:48:53 +0200

    fix petrol

--- Driver.m
+++ Driver.m
@@ -3,13 +3,26 @@ classdef Driver < Client
     properties
         FuelInlet
         FuelPump
+        EndTime
     end
 
     methods 
-        function obj = Driver(FuelInlet)
-            obj@Client();
-            obj.FuelInlet = FuelInlet;
-            obj.Pump = NaN;
+        function obj = Driver(Id, EnterTime)
+            obj@Client(Id, EnterTime);
+            obj.FuelInlet = randi([1,2]);
+            obj.FuelPump = NaN;
+            obj.EndTime = inf;
+        end
+
+        function CleanState(obj)
+            obj.Id = NaN;
+            obj.EnterTime = NaN;
+            obj.FuelInlet = NaN;
+            obj.FuelPump = NaN;
+        end
+
+        function EndRefill(obj, EndTime)
+            obj.EndTime = EndTime;
         end
     end
 end
--- FuelServiceStation.m
+++ FuelServiceStation.m
@@ -1,16 +1,19 @@
 classdef FuelServiceStation < Event
 
         methods
-            function Manage(obj,Sim) 
-                    Sim.Clock = obj.Next;
-                    Sim.ServiceQueue.Update();
-                    if Sim.ServiceQueue.NumInQueue >0
-                        Sim.WaitingTimeCash.AddJoinTime(obj.Next);
-                    else
-                        Sim.CashService.GenerateNext(); % ho servito un altro cliente 
-
-                    end
-
+            function Manage(obj,Sim)
+                idx = cellfun(@(c) c.EndTime == obj.Next, Sim.ClientQueue.ClientsList);
+                Client = Sim.ClientQueue.ClientsList{idx};
+                
+                Sim.ServiceQueue.UpdateQueue(Client);
+                if Sim.ServiceQueue.NumInQueue > 1
+                    Sim.WaitingTimeCash.AddJoinTime(obj.Next);
+                else
+                    Sim.CashService.GenerateNext(); % ho servito un altro cliente 
+                end
+                if Sim.ClientQueue.NumInQueue == 1
+                    obj.Reset();
+                end
             end
         end
         
