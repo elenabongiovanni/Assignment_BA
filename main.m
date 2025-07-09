2fcc9f5fae0a49e15ed2982b0d82889dc80db4cf
Elena Bongiovanni <s293968@studenti.polito.it>
2025-07-10 00:45:27 +0200

    modifica main

--- main.m
+++ main.m
@@ -4,7 +4,8 @@ close all
 
 rng(1);
 
-Simulation = SimulationRollQueue(100,10,5);
-%Simulation = SimulationPetrolStation(1000, 3 ,[2,5], [1,2], 8, 2, 2);
+SimulationRoll = SimulationRollQueue(100,10,5);
+SimulationStation = SimulationPetrolStation(1000, 3 ,[2,5], [1,2], 8, 2, 2);
 
-Simulation.StartSimulation()
+SimulationRoll.StartSimulation();
+SimulationStation.StartSimulation();
